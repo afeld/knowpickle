0cd407792e7cf6c9636c153635b8f07f86139835
Aidan Feldman <aidan.feldman@gmail.com>
2011-01-27 00:57:24 -0500

    converted some views to HAML and improved them

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,4 +2,12 @@ class Resource < ActiveRecord::Base
   has_many :reviews, :dependent => :destroy
   belongs_to :topic
   belongs_to :creator, :class_name => 'User'
+  
+  def to_s
+    self.name
+  end
+  
+  def average_rating
+    self.reviews.average :rating
+  end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :resources, :dependent => :nullify
+  
+  def to_s
+    self.name
+  end
 end
