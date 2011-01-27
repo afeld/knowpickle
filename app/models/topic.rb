685f2b4e5745ee60cc179e3ba9ac621fb3a299fb
Aidan Feldman <aidan.feldman@gmail.com>
2011-01-27 03:52:23 -0500

    refactored Topic tree into partial

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ActiveRecord::Base
   default_value_for :enabled, false
-  default_scope where(:enabled => true)
+  default_scope order(:name).where(:enabled => true)
   
   has_many :resources, :dependent => :nullify
   
