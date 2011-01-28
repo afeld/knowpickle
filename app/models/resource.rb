64861f8c4bb35c41eaeccfe56f49591bf58dc14c
Aidan Feldman <aidan.feldman@gmail.com>
2011-01-28 03:19:32 -0500

    validate URL is present and retrieve title

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,6 +3,9 @@ class Resource < ActiveRecord::Base
   belongs_to :topic
   belongs_to :contributor, :class_name => 'User'
   
+  validate :url, :presence => true # TODO this won't always be the case
+  after_validation :validate_url_and_fetch_title
+  
   def to_s
     self.name
   end
@@ -15,5 +18,33 @@ class Resource < ActiveRecord::Base
   def site
     # TODO make new model for Site
     URI.parse(self.url).host || self.url
+  end
+  
+  
+  private
+
+  def validate_url_and_fetch_title
+    begin
+      end_response = RestClient.get(self.url) { |response, request, result, &block|
+        case response.code
+        when 301, 302, 307
+          # it's a redirect, so update the URL
+          self.url = response.headers[:location]
+          response.follow_redirection(request, result, &block)
+        when 404
+          errors.add(:url, "cannot be found")
+        else
+          response.return!(request, result, &block)
+        end
+      }
+    rescue
+      errors.add(:url, 'is invalid')
+    end
+    
+    if end_response and self.name.empty?
+      doc = Nokogiri::HTML(end_response)
+      title = doc.xpath('//head/title[1]').first.text rescue nil
+      self.name = title
+    end
   end
 end
