99778ddaee377195b514d701a8279a74af1b1a96
Aidan Feldman <aidan.feldman@gmail.com>
2011-02-03 02:26:42 -0500

    more editable Resource fields

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,2 +1,11 @@
 module ResourcesHelper
+  def levels_json_string
+    # some funky Array->Hash conversion, courtesy of http://snippets.dzone.com/posts/show/302
+    # Hash[*LEVELS.collect{|l| [l,l]}.flatten].to_json
+    
+    # TODO move this logic to jeditable-rails
+    levels_pairs = ["'': 'Select level...'"]
+    levels_pairs.concat LEVELS.collect{|l| "\"#{l}\": \"#{l}\""}
+    "{#{levels_pairs.join(', ')}}"
+  end
 end
