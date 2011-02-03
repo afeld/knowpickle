0b1f4542ef3689d712aff560564a67fd00d7b9b6
Aidan Feldman <aidan.feldman@gmail.com>
2011-02-03 00:24:44 -0500

    using jeditable-rails (my new gem)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,36 +1,22 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-var edit_mode = false;
-
-function makeEditable(model) {
-  $('.edit').each(function(){
-    var self = $(this);
-    var name = model + '[' + self.attr('id') + ']';
-    self.editable(window.location.toString(), {
-      method: 'PUT',
-      name: name
-      /*
-      data: function(value, settings) {
-        // Remove surrrounding anchor tags
-        return value.replace(/<\/?a[^>]*>/gi, '');
-      }
-      */
-    });
-  });
-  
-  toggleEdit();
-}
+var editMode = false;
 
 function toggleEdit() {
-  if (edit_mode) {
-    $('.edit').removeClass('active').editable('disable');
-    $('.toggle-edit').removeClass('active');
+  editMode = !editMode;
+  if (editMode) {
+    enableEdit();
   } else {
-    $('.edit').addClass('active').editable('enable');
-    $('.toggle-edit').addClass('active');
+    $('.editable').removeClass('active').editable('disable');
+    $('.toggle-edit').removeClass('active');
   }
-  edit_mode = !edit_mode;
+}
+
+function enableEdit() {
+  editMode = true;
+  $('.editable').addClass('active').editable('enable');
+  $('.toggle-edit').addClass('active');
 }
 
 $(function() {
