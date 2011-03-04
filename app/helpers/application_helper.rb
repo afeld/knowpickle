6d8d4698fd4f987e6d28ae37df77d565bfecdc1d
Aidan Feldman <aidan.feldman@gmail.com>
2011-03-04 06:04:37 -0500

    changed topic display on homepage to grid

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     text = current_user.display_name.present? ? current_user.display_name : current_user.email
     link_to text, user_path(current_user), :class => 'display-name'
   end
+  
+  def grid_class(count)
+    if count % 4 == 0
+      'alpha'
+    elsif count % 4 == 3
+      'omega'
+    end
+  end
 end
